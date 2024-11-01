62b8ee8bbc991bf0788cdcae74cba91c317f0767
Cansu Aksu <cansua@amazon.com>
2024-11-01 14:11:44 +0100

    fix: Override iconName documenter value

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -6,5 +6,13 @@ import { getAllComponents, requireComponentDefinition } from "./utils";
 
 test.each<string>(getAllComponents())(`definition for %s matches the snapshot`, (componentName: string) => {
   const definition = requireComponentDefinition(componentName);
+
+  // overriding with a fake value so that when there are icon changes in components this test doesn't block it
+  if (componentName === "avatar") {
+    const iconNameDefinition = definition.properties.filter((def) => def.name === "iconName");
+    if (iconNameDefinition && iconNameDefinition[0]) {
+      iconNameDefinition[0].inlineType.values = "comes from @cloudscape-design/components";
+    }
+  }
   expect(definition).toMatchSnapshot(componentName);
 });
