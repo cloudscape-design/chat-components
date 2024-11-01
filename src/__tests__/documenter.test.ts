3459e1f7d37543b0dcee61da00ba394c87d10941
Cansu Aksu <cansua@amazon.com>
2024-11-01 14:30:59 +0100

    remove if check

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -8,11 +8,9 @@ test.each<string>(getAllComponents())(`definition for %s matches the snapshot`, 
   const definition = requireComponentDefinition(componentName);
 
   // overriding with a fake value so that when there are icon changes in components this test doesn't block it
-  if (componentName === "avatar") {
-    const iconNameDefinition = definition.properties.filter(({ name }: { name: string }) => name === "iconName");
-    if (iconNameDefinition && iconNameDefinition[0]) {
-      iconNameDefinition[0].inlineType.values = "comes from @cloudscape-design/components";
-    }
+  const iconNameDefinition = definition.properties.filter(({ name }: { name: string }) => name === "iconName");
+  if (iconNameDefinition && iconNameDefinition[0]) {
+    iconNameDefinition[0].inlineType.values = "comes from @cloudscape-design/components";
   }
   expect(definition).toMatchSnapshot(componentName);
 });
