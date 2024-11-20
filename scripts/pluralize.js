aefa6dabdee7d032b73fc0b0b7fe455184b98ca4
Amir Alami <abstract.stream@gmail.com>
2024-11-20 17:05:17 +0100

    feat: Adds findAll finder to function (#34)

---
+++ scripts/pluralize.js
@@ -0,0 +1,17 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+const pluralizationMap = {
+  Avatar: "Avatars",
+  ChatBubble: "ChatBubbles",
+  LoadingBar: "LoadingBars",
+};
+
+function pluralizeComponentName(componentName) {
+  if (!(componentName in pluralizationMap)) {
+    throw new Error(`Could not find the plural case for ${componentName}.`);
+  }
+
+  return pluralizationMap[componentName];
+}
+
+export { pluralizeComponentName };
