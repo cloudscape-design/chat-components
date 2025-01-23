973e482aa0301738062a988700049bec19c2a6cb
Katie George <katiegeo@amazon.com>
2025-01-23 15:24:08 -0800

    chore: Update test page

--- pages/support-prompt-group/in-context.page.tsx
+++ pages/support-prompt-group/in-context.page.tsx
@@ -1,5 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import { useState } from "react";
+
+import Button from "@cloudscape-design/components/button";
 import Container from "@cloudscape-design/components/container";
 import Header from "@cloudscape-design/components/header";
 import PromptInput from "@cloudscape-design/components/prompt-input";
@@ -12,42 +15,50 @@ import { ChatBubbleAvatarGenAI, ChatBubbleAvatarUser } from "../chat-bubble/util
 import { ScreenshotArea } from "../screenshot-area";
 
 export default function SupportPromptPage() {
+  const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
+  const [sentText, setSentText] = useState("");
+
+  const items = [
+    {
+      text: "Create a really detailed and powerful image. The image should be of a mountain scene with a blue lake and green hills, with a sunset in the background. In the lake, there should be 3 whales leaping out of the water.",
+      id: "image",
+    },
+    {
+      text: "Help me brainstorm for an upcoming sign-off.",
+      id: "brainstorm",
+    },
+    {
+      text: "Summarize this long and complex PDF for me. Include a paragraph containing 3-4 sentences that capture the main ideas and overall message of the documents, a list of 5 to 10 key points from the document, and up to 3 follow-up questions that arise from the content of the document.",
+      id: "summarize",
+    },
+  ];
+
+  const items2 = [
+    {
+      text: "Create a really detailed and powerful image.",
+      id: "image",
+    },
+    {
+      text: "Help me brainstorm for an upcoming sign-off.",
+      id: "brainstorm",
+    },
+    {
+      text: "Summarize this long and complex PDF for me.",
+      id: "summarize",
+    },
+  ];
+
   return (
     <ScreenshotArea>
       <main style={{ maxWidth: "800px" }}>
         <TestBed>
           <SpaceBetween size="xl">
-            <Container header={<Header>Image generator</Header>}>
-              <SpaceBetween direction="vertical" size="xxl">
-                <div style={{ height: "100px" }} />
-
-                <SpaceBetween direction="vertical" size="m">
-                  <SupportPromptGroup
-                    alignment="horizontal"
-                    ariaLabel="Horizontal support prompt group"
-                    onItemClick={({ detail }) => console.log(detail)}
-                    items={[
-                      {
-                        text: "Create a really detailed and powerful image.",
-                        id: "image",
-                      },
-                      {
-                        text: "Help me brainstorm for an upcoming sign-off.",
-                        id: "brainstorm",
-                      },
-                      {
-                        text: "Summarize this long and complex PDF for me.",
-                        id: "summarize",
-                      },
-                    ]}
-                  />
-
-                  <PromptInput placeholder="Write a prompt" value="" actionButtonIconName="send" />
-                </SpaceBetween>
-              </SpaceBetween>
-            </Container>
-
-            <Container header={<Header>Image generator</Header>}>
+            <Container
+              header={
+                <Header actions={<Button onClick={() => setText("")}>Reset</Button>}>Support prompt test: send</Header>
+              }
+            >
               <SpaceBetween direction="vertical" size="xxl">
                 <div>
                   <ChatBubble type="outgoing" avatar={<ChatBubbleAvatarUser />} ariaLabel="User at 4:23:20pm">
@@ -58,36 +69,69 @@ export default function SupportPromptPage() {
                       Amazon S3 provides a simple web service interface that you can use to store and retrieve any
                       amount of data, at any time, from anywhere.
                     </ChatBubble>
-                    <div style={{ marginInlineStart: "36px" }}>
-                      <SupportPromptGroup
-                        ariaLabel="Horizontal support prompt group"
-                        alignment="vertical"
-                        onItemClick={({ detail }) => console.log(detail)}
-                        items={[
-                          {
-                            text: "Create a really detailed and powerful image. The image should be of a mountain scene with a blue lake and green hills, with a sunset in the background. In the lake, there should be 3 whales leaping out of the water.",
-                            id: "image",
-                          },
-                          {
-                            text: "Help me brainstorm for an upcoming sign-off.",
-                            id: "brainstorm",
-                          },
-                          {
-                            text: "Summarize this long and complex PDF for me. Include a paragraph containing 3-4 sentences that capture the main ideas and overall message of the documents, a list of 5 to 10 key points from the document, and up to 3 follow-up questions that arise from the content of the document.",
-                            id: "summarize",
-                          },
-                          {
-                            text: "What  questions remain unanswered after reading the document(s)? The response shall consider all current or past uploaded documents.",
-                            id: "image",
-                          },
-                        ]}
-                      />
-                    </div>
+                    {text === "" && (
+                      <div style={{ marginInlineStart: "36px" }}>
+                        <SupportPromptGroup
+                          ariaLabel="Horizontal support prompt group"
+                          alignment="vertical"
+                          onItemClick={({ detail }) => {
+                            const activeItem = items.find((item) => item.id === detail.id);
+                            setText(activeItem?.text || "");
+                            console.log(detail);
+                          }}
+                          items={items}
+                        />
+                      </div>
+                    )}
                   </SpaceBetween>
+                  {text !== "" && (
+                    <ChatBubble type="outgoing" avatar={<ChatBubbleAvatarUser />} ariaLabel="User at 4:23:20pm">
+                      {text}
+                    </ChatBubble>
+                  )}
                 </div>
 
                 <SpaceBetween direction="vertical" size="m">
                   <PromptInput placeholder="Write a prompt" value="" actionButtonIconName="send" />
+                </SpaceBetween>
+              </SpaceBetween>
+            </Container>
+            <Container
+              header={
+                <Header actions={<Button onClick={() => setSentText("")}>Reset</Button>}>
+                  Support prompt test: draft
+                </Header>
+              }
+            >
+              <SpaceBetween direction="vertical" size="m">
+                <div style={{ height: "100px" }} />
+                {sentText !== "" && (
+                  <ChatBubble type="outgoing" avatar={<ChatBubbleAvatarUser />} ariaLabel="User at 4:23:20pm">
+                    {sentText}
+                  </ChatBubble>
+                )}
+                <SpaceBetween direction="vertical" size="m">
+                  {sentText === "" && (
+                    <SupportPromptGroup
+                      alignment="horizontal"
+                      ariaLabel="Horizontal support prompt group"
+                      onItemClick={({ detail }) => {
+                        const activeItem = items2.find((item) => item.id === detail.id);
+                        setText2(activeItem?.text || "");
+                      }}
+                      items={items2}
+                    />
+                  )}
+
+                  <PromptInput
+                    placeholder="Write a prompt"
+                    value={text2}
+                    actionButtonIconName="send"
+                    onAction={() => {
+                      setSentText(text2);
+                      setText2("");
+                    }}
+                  />
                 </SpaceBetween>
               </SpaceBetween>
             </Container>
