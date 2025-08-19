6f347013ed42c16f75182c0d9544ef65a8dd5b1a
Michael Dowse <dowsem@amazon.com>
2025-08-19 15:49:49 +0200

    fixes

--- scripts/themeable-source.js
+++ scripts/themeable-source.js
@@ -6,14 +6,13 @@ import path from "node:path";
 import process from "node:process";
 const cwd = process.cwd();
 
-const targetDir = path.join(cwd, "./lib/components-themeable/internal");
+const targetDir = path.join(cwd, "./lib/components-themeable");
 
 const stylesSourceDir = path.join(cwd, "./src/");
-const stylesTargetDir = path.join(targetDir, "/scss");
+const stylesTargetDir = path.join(targetDir, "internal/scss");
 
 const componentsSourceDir = path.join(cwd, "./lib/components");
-const componentsTargetDir = path.join(targetDir, "/template");
-const designTokensTemplateDir = path.join(targetDir, "/template-tokens");
+const componentsTargetDir = path.join(targetDir, "internal/template");
 
 function copyStyles() {
   for (const file of globbySync("**/*.scss", { cwd: stylesSourceDir })) {
@@ -33,12 +32,11 @@ function copyStyles() {
 function copyTemplate() {
   fs.mkdirSync(componentsTargetDir, { recursive: true });
   fs.cpSync(componentsSourceDir, componentsTargetDir, { recursive: true });
-  fs.mkdirSync(designTokensTemplateDir, { recursive: true });
   fs.writeFileSync(
-    path.join(designTokensTemplateDir, "package.json"),
+    path.join(componentsTargetDir, "package.json"),
     JSON.stringify(
       {
-        name: "@cloudscape-design/design-tokens-themed",
+        name: "@cloudscape-design/chat-components-themed",
         version: "1.0.0",
       },
       null,
