d9d37dc08729cd70a6748f63d95d8d2bda219cd0
Joan Perals Tresserra <jotresse@amazon.com>
2025-12-12 20:08:05 +0100

    Do not exclude React

--- vite.config.dev-pages.mjs
+++ vite.config.dev-pages.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
     },
     outDir: "lib/dev-pages/bundle",
     rollupOptions: {
-      external: [componentsPath, /^@cloudscape-design\/*/, "react"],
+      external: [componentsPath, /^@cloudscape-design\/*/],
     },
   },
 });
