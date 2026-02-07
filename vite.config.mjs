b65a843d34352825961c9049d607ef071ae4818a
Joan Perals Tresserra <jotresse@amazon.com>
2026-02-07 23:48:21 +0100

    View config workaround

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +16,15 @@ export default defineConfig({
   resolve: {
     alias: {
       lodash: "lodash-es",
+      "@cloudscape-design/components/internal/components/card": path.resolve(
+        __dirname,
+        "node_modules",
+        "@cloudscape-design",
+        "components",
+        "internal",
+        "components",
+        "card",
+      ),
     },
   },
   server: {
