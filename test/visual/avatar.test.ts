3df03e023056f65d0613b2147c91292d2a2eb7d1
Joan Perals Tresserra <jotresse@amazon.com>
2026-04-21 19:03:38 +0200

    Fix imports

--- test/visual/avatar.test.ts
+++ test/visual/avatar.test.ts
@@ -1,9 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { describe } from "vitest";
+import { describe, test } from "vitest";
 
 import compareScreenshots from "../compare-screenshots";
-import { test } from "../visual-test-setup";
 
 describe("Avatar", () => {
   test("custom style", () =>
