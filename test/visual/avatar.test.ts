8ed3d2ce7d6cb592a0f1a43d7c74a7e99fe900a6
Joan Perals Tresserra <jotresse@amazon.com>
2026-04-21 18:21:37 +0200

    Refactor

--- test/visual/avatar.test.ts
+++ test/visual/avatar.test.ts
@@ -5,19 +5,17 @@ import { describe } from "vitest";
 import { test } from "../visual-test-setup";
 
 describe("Avatar", () => {
-  test("custom style", ({ compareScreenshots }) => {
+  test("custom style", ({ compareScreenshots }) =>
     compareScreenshots(async (page) => {
       await page.openScenario("avatar/custom-style");
       return page.captureScreenshotArea();
-    })();
-  });
+    }));
 
-  test("permutations", ({ compareScreenshots }) => {
+  test("permutations", ({ compareScreenshots }) =>
     compareScreenshots(async (page) => {
       await page.openScenario("avatar/permutations");
       // The page does not actually use our permutations component,
       // therefore we capture the entire screenshot area at once.
       return page.captureScreenshotArea();
-    })();
-  });
+    }));
 });
