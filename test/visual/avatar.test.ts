3abb309e3d7e70f23db0c3f502c1adcdedfd0456
Joan Perals Tresserra <jotresse@amazon.com>
2026-04-21 18:58:54 +0200

    Remove context-based vitest config

--- test/visual/avatar.test.ts
+++ test/visual/avatar.test.ts
@@ -2,16 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 import { describe } from "vitest";
 
+import compareScreenshots from "../compare-screenshots";
 import { test } from "../visual-test-setup";
 
 describe("Avatar", () => {
-  test("custom style", ({ compareScreenshots }) =>
+  test("custom style", () =>
     compareScreenshots(async (page) => {
       await page.openScenario("avatar/custom-style");
       return page.captureScreenshotArea();
     }));
 
-  test("permutations", ({ compareScreenshots }) =>
+  test("permutations", () =>
     compareScreenshots(async (page) => {
       await page.openScenario("avatar/permutations");
       // The page does not actually use our permutations component,
