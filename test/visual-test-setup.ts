0444812d2a341a65024edfaa82dfc113ad64ba7d
Joan Perals Tresserra <jotresse@amazon.com>
2026-04-21 17:49:03 +0200

    Add visual test for Avatar

--- test/visual-test-setup.ts
+++ test/visual-test-setup.ts
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 import { configureToMatchImageSnapshot } from "jest-image-snapshot";
 import { join } from "path";
-import { expect } from "vitest";
+import { expect, test as baseTest } from "vitest";
+
+import compareScreenshots from "./compare-screenshots";
 
 const snapshotDir = join(__dirname, "./..", process.env.VISUAL_REGRESSION_SNAPSHOT_DIRECTORY ?? "__image_snapshots__");
 
@@ -11,3 +13,12 @@ const toMatchImageSnapshot = configureToMatchImageSnapshot({
 });
 
 expect.extend({ toMatchImageSnapshot });
+
+const test = baseTest.extend<{ compareScreenshots: typeof compareScreenshots }>({
+  // eslint-disable-next-line no-empty-pattern
+  compareScreenshots: async ({}, use) => {
+    await use(compareScreenshots);
+  },
+});
+
+export { test };
---
+++ test/visual/avatar.test.ts
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { describe } from "vitest";
+
+import { test } from "../visual-test-setup";
+
+describe("Avatar", () => {
+  test("custom style", ({ compareScreenshots }) => {
+    compareScreenshots(async (page) => {
+      await page.openScenario("avatar/custom-style");
+      return page.captureScreenshotArea();
+    })();
+  });
+
+  test("permutations", ({ compareScreenshots }) => {
+    compareScreenshots(async (page) => {
+      await page.openScenario("avatar/permutations");
+      // The page does not actually use our permutations component,
+      // therefore we capture the entire screenshot area at once.
+      return page.captureScreenshotArea();
+    })();
+  });
+});
