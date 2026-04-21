6b48b2a0afa8ed2e3a74eb0d2566c9d618d72cdf
Joan Perals Tresserra <jotresse@amazon.com>
2026-04-21 18:42:59 +0200

    Add import

--- test/compare-screenshots.ts
+++ test/compare-screenshots.ts
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import { expect } from "vitest";
 
 import { ScreenshotWithOffset } from "@cloudscape-design/browser-test-tools/page-objects";
 import useBrowser from "@cloudscape-design/browser-test-tools/use-browser";
