5838b9e22e942eab6ef1ce0da44bf4a2181a70d7
Maximilian Schöll <20025802+mxschll@users.noreply.github.com>
2026-07-29 12:48:07 +0200

    refactor: apply dev-page modes via global-styles callback (#153)
    Co-authored-by: Maximilian Schoell <mxschll@users.noreply.github.com>

--- pages/app/screenshot-area.tsx
+++ pages/app/screenshot-area.tsx
@@ -1,14 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ReactNode, useContext } from "react";
+import { ReactNode } from "react";
 import clsx from "clsx";
 
-import AppContext from "./app-context";
+import { useAppModes } from "@cloudscape-design/build-tools/lib/dev-pages-utils";
 
 import styles from "./screenshot-area.module.css";
 
 export function ScreenshotArea({ children }: { children: ReactNode }) {
-  const { urlParams } = useContext(AppContext);
+  const { urlParams } = useAppModes();
   return <div className={clsx("screenshot-area", urlParams.screenshotMode && styles["no-animation"])}>{children}</div>;
 }
