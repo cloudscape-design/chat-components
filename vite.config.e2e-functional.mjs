2e6bf998d3410aeb0f93390303d30e03e3985922
Andrei Zhaleznichenka <zhalea@amazon.de>
2026-09-17 11:55:10 +0000

    chore: Eslint upgrade (#176)

--- vite.config.e2e-functional.mjs
+++ vite.config.e2e-functional.mjs
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { defineConfig } from "vite";
+
 import base from "./vite.config.mjs";
 
 // https://vitejs.dev/config/
